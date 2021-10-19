f05cde03b5d420348b573cedc9f95ca2f0799f70
Preston Foote <prestonwfoote@gmail.com>
2021-10-19 17:22:26 -0400

    add ablility to add task

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+var buttonEl = document.querySelector("#save-task");
+console.log(buttonEl);
+
+buttonEl.addEventListener("click", function (){
+    alert("button clicked");
+});
+
+var buttonEl = document.querySelector("#save-task"); 
+var tasksToDoEl = document.querySelector("#tasks-to-do"); 
+
+var createTaskHandler = function() { 
+  var listItemEl = document.createElement("li"); 
+  listItemEl.className = "task-item"; 
+  listItemEl.textContent = "This is a new task."; 
+  tasksToDoEl.appendChild(listItemEl); 
+  }; 
+
+buttonEl.addEventListener("click", createTaskHandler);
